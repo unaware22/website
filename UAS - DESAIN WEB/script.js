9110b94255e65cfb89783d99ce9ff38f068ea61f
Aditya Nur Lintang <159440750+unaware22@users.noreply.github.com>
2024-11-25 11:47:52 +0700

    Delete UAS - DESAIN WEB directory

--- UAS - DESAIN WEB/script.js
+++
@@ -1,7 +0,0 @@
-const hamburgerBtn = document.getElementById('navbar-btn');
-const mobileMenu = document.getElementById('mobile-menu');
-
-hamburgerBtn.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden');
-    
-});
